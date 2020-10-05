8ecefeefe83203167eb1bd648c8bcbd8396d41ea
José Fernando <jose.gomezm12@gmail.com>
2020-10-05 01:21:53 -0500

    semicolons & types

--- src/resolvers/category.ts
+++ src/resolvers/category.ts
@@ -1,35 +1,36 @@
 import { Category } from '../entity/Category';
-import { combineResolvers } from 'graphql-resolvers'
+import { combineResolvers } from 'graphql-resolvers';
 import { isAuthenticated } from './middleware';
 
 export = {
   Query: {
     getCategories: combineResolvers(isAuthenticated, async (): Promise<Category[]> => {
       try {
-        const categories = await Category.find()
-        return categories
+        const categories : Category[]= await Category.find();
+        return categories;
       } catch(e) {
-        return e
+        return e;
       }
     }),
     getOneCategory: combineResolvers(isAuthenticated, async (_ : Object, { id } : any): Promise<Category | undefined> => {
       try {
-        const category = await Category.findOne({id})
-        return category
-      } catch(e) {
+        const category : Category | undefined = await Category.findOne({id});
+        return category;
+      } catch(error) {
+        return error;
       }
     })
   },
   Mutation: {
     createCategory: combineResolvers(isAuthenticated, async (_ : any, { input } : any): Promise<Category[]> => {
       try {
-        const newCategory = Category.create(input)
-        await Category.insert(newCategory)
-        console.log(newCategory)
-        return newCategory
-      } catch(e) {
-        console.log(e)
-        return e
+        const newCategory = Category.create(input);
+        await Category.insert(newCategory);
+        console.log(newCategory);
+        return newCategory;
+      } catch(error) {
+        console.log(error);
+        return error;
       }
     })
   }
